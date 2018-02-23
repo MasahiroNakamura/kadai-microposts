d5748a68369913dd92c963590a05e1850f357db2
Masahiro.Nakamura <nakamuramasakayo@me.com>
2018-02-23 07:15:40 +0000

    post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました'
+    redirect_to root_url
   end
   
   private
@@ -30,9 +33,4 @@ class SessionsController < ApplicationController
       return false
     end
   end
-  
-  def destroy
-    session[:user_id] = nil
-    flash[:success] = 'ログアウトしました。'
-    redirect_to root_url
 end

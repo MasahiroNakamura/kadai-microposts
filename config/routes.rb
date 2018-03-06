32ec143a4610d2e59c08e47df26c3c2fe19397f8
Masahiro.Nakamura <nakamuramasakayo@me.com>
2018-03-06 14:02:37 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'relationships/create'
+
+  get 'relationships/destroy'
+
   get 'microposts/create'
 
   get 'microposts/destroy'
@@ -17,7 +21,16 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+    collection do
+      get :search
+    end
+  end
   
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
